59dc16a8f7338e6283247254fa6da3875375a77f
Thomas Lahore <tom.lahore@gmail.com>
2023-10-24 09:18:36 -0700

    bigger population and added logging metric and hidden dim

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ deterministic_matches = False
 allow_model_rng_access = False
 warmup_rounds = 0
 total_rounds = 150
-popsize = 250
+popsize = 500  # 250
 generations = 1000
 log_interval = 1
 stdev_init = 10.0
@@ -12,6 +12,9 @@ initial_bounds = 5.0
 ROCK = 0
 PAPER = 1
 SCISSORS = 2
+# mean_eval | pop_best_eval | median_eval | best_eval | worst_eval
+visualization_metric = 'median_eval'
+hidden_dim = 25
 
 # allows the introduction of asymmetries that destabilize
 #  the trivial always-random strategy
