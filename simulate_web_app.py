69f069d04784212a7820935d07b22d1162494399
Thomas Lahore <tom.lahore@gmail.com>
2023-10-24 16:39:20 -0700

    add sessions to webapp for multiple simultaneous players

--- simulate_web_app.py
+++ simulate_web_app.py
@@ -1,43 +1,59 @@
-from bottle import route, run, request, response, static_file
+from bottle import Bottle, request, response, static_file, run
+from beaker.middleware import SessionMiddleware
+import uuid
 from utils import load_champion
 from config import choice_to_id, id_to_choice
 
-ai = load_champion()
+app = Bottle()
 
-# Route to serve the main page
-@route('/')
+# Set up the session options
+session_opts = {
+    'session.type': 'memory',
+    'session.cookie_expires': 3600,  # 1 hour
+    'session.auto': True
+}
+
+@app.route('/')
 def index():
     return static_file('index.html', root='static/html/')
 
-# Route to serve JavaScript files
-@route('/js/<filename>')
+@app.route('/js/<filename>')
 def serve_js(filename):
     return static_file(filename, root='static/js/')
 
-# Route to serve CSS files
-@route('/css/<filename>')
+@app.route('/css/<filename>')
 def serve_css(filename):
     return static_file(filename, root='static/css/')
 
-# Route to serve images
-@route('/images/<filename>')
+@app.route('/images/<filename>')
 def serve_images(filename):
     return static_file(filename, root='static/images/')
 
-# Game logic
-@route('/play/<player_choice>', method='GET')
+@app.route('/play/<player_choice>', method='GET')
 def play_rps(player_choice):
+    s = request.environ['beaker.session']
+    user_id = s.get('user_id')
+
+    # If the user doesn't have an ID, create one and initialize their AI instance
+    if not user_id:
+        user_id = str(uuid.uuid4())
+        s['user_id'] = user_id
+        s['ai'] = load_champion()
+        s.save()
+
+    # Retrieve the AI instance from the session
+    user_ai = s['ai']
+
     if player_choice not in ["rock", "paper", "scissors"]:
         response.status = 400
         return {"error": "Invalid choice"}
-    ai_choice_id = ai.move(choice_to_id[player_choice])
+
+    ai_choice_id = user_ai.move(choice_to_id[player_choice])
     return {
         "ai_choice": id_to_choice[ai_choice_id]
     }
 
 
 if __name__ == "__main__":
-    """
-    Play against current champion on localhost
-    """
-    run(host='localhost', port=8080)
+    app_with_sessions = SessionMiddleware(app, session_opts)
+    run(app=app_with_sessions, host='localhost', port=8080)
