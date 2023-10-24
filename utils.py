92acbba78caa8559002aacff376609bbda4bd970
Thomas Lahore <tom.lahore@gmail.com>
2023-10-24 15:36:45 -0700

    parameterize how we load champion

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import glob
 import pickle
 from config_agent import player1_class
+from config import champion_type
 
 
 def load_champion():
     path = sorted(glob.glob('data/*.pickle'))[-1]
     with open(path, 'rb') as f:
         solution = pickle.load(f)
-    x = solution['best']  # 'center'
+    x = solution[champion_type]
     dim = player1_class().get_dim()
     assert len(x) == dim, 'dimension mismatch'
     champion = player1_class()
